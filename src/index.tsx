fe3df99bd1aadb687f9550097f9263715dd186a8
Sergey Mironenko <serg1prime@gmail.com>
2024-05-01 11:57:28 +0300

    Auth-Client: sixteenth commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { HashRouter as Router } from 'react-router-dom';
 import store from './app/store';
 import { App } from './App';
 import reportWebVitals from './reportWebVitals';
-import './App.scss';
+import './styles/styles.scss';
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
